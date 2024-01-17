4c98e162a627adfbf111e0ec303eed86dbcd6b34
HanJu <119592507+dev1week@users.noreply.github.com>
2024-01-17 23:51:40 +0900

    Title: 차이가 가장 작은 수, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240117/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 240117/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -10,38 +10,25 @@ public class Main {
         int n = Integer.valueOf(tokens.nextToken());
         long m = Long.valueOf(tokens.nextToken());
 
-        TreeSet<Long> offsets = new TreeSet<>(); 
         long[] numbers = new long[n];
-        //n개의 정수를 배열에 입력 받는다. 
+        TreeSet<Long> datas = new TreeSet<>();
+
         for(int i=0; i<n; i++){
             numbers[i] = Long.valueOf(buffer.readLine());
+            datas.add(numbers[i]); 
         }
-        
 
-        
-        //n개의 정수를 정렬한다.
-        Arrays.sort(numbers);
-        //모든 차이를 구한다. => 트리에 때려 박는다. 
 
-        for(int i=n-1; i>=0; i--){
-            for(int j=i; j>=0; j--){
-                if(i!=j){
-                    long offset = numbers[i] - numbers[j]; 
-                    offsets.add(offset); 
+        long result = Long.MAX_VALUE; 
+        for(long number : numbers){
+            if(datas.higher(number+m)!=null){
+                if(result>datas.higher(number+m)-number){
+                    result = datas.higher(number+m)-number; 
                 }
             }
         }
-        
 
-        if(offsets.ceiling(m)!=null){
-            System.out.println(offsets.ceiling(m));
-        }else{
-            System.out.println(-1); 
-        }
-
-        //트리.ceiling(m)이 있는지 확인 
-            //있으면 바로 출력 
-            //없으면 -1 출력 
+        System.out.println(result); 
 
     }
    
