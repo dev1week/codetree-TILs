a32a35a8a2499da2aea8d5f27cb11f0c859c88c9
HanJu <119592507+dev1week@users.noreply.github.com>
2024-07-21 15:27:17 +0900

    Title: 이진 트리 최대 경로 합, Time: 480ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240721/이진 트리 최대 경로 합/binary-tree-maximum-path-sum.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*; 
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+    static int[] distance; 
+
+    static final int ROOT = 1; 
+    
+    static List<Integer>[] graph;
+    static int[] nodes; 
+
+    public static void main(String[] args) throws IOException{
+        int numOfNode = Integer.parseInt(buffer.readLine());
+
+        distance = new int[numOfNode+1];
+        graph = new List[numOfNode+1];
+        nodes = new int[numOfNode+1];
+        for(int node=1; node<=numOfNode; node++){
+            graph[node] = new ArrayList<>(); 
+        }
+
+        for(int edge=0; edge<numOfNode-1; edge++){
+            tokens = new StringTokenizer(buffer.readLine());
+            int start = Integer.parseInt(tokens.nextToken());
+            int end = Integer.parseInt(tokens.nextToken());
+
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+
+        for(int node=1; node<=numOfNode; node++){
+            nodes[node] = Integer.parseInt(buffer.readLine());
+        }
+
+        dfs(ROOT, -1);
+        int max = Arrays.stream(distance).max().orElseThrow(NoSuchElementException::new);
+        System.out.println(max);
+    }
+
+
+    private static void dfs(int current, int parent){
+        distance[current] = 0; 
+        int childTotal = 0; 
+        for(int child :graph[current]){
+            if(parent == child) continue; 
+
+            dfs(child, current); 
+
+            distance[current] = Math.max(distance[current], distance[child]);
+            childTotal+= distance[child];
+        }
+
+        distance[current] = Math.max(distance[current], childTotal)+ nodes[current]; 
+        //자식이 하나일 경우 
+
+        //자식이 두개일 경우 
+
+
+        //자식이 0개일 경우 
+
+    }
+}
