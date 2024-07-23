af114e00046899b1ba66a4c844e47b4c267f1513
HanJu <119592507+dev1week@users.noreply.github.com>
2024-07-23 10:59:48 +0900

    Title: 사각형 채우기 3, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,25 @@
+import java.io.*; 
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    //d[i] n=i일 때 가능한 경우의 수 
+
+    //d[i] = 2*d[i-1] + 2*d[i-2];  
+
+    public static void main(String[] args) throws IOException{
+        int n = Integer.valueOf(buffer.readLine());
+        long[] d = new long[1001];
+
+        d[1] = 2;
+        d[2] = 7;
+        d[3] = 22; 
+
+
+        for(int i=4; i<=1000; i++){
+            d[i] = d[i-1]*3 + d[i-2]*2 + d[i-3]*2;
+            d[i] %= 1000000007;
+        }
+        System.out.println(d[n]);
+
+    }
+}
