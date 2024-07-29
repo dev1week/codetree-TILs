6995692c7c1da0b27c9b64bbcaddf4b3f03e9924
HanJu <119592507+dev1week@users.noreply.github.com>
2024-07-29 17:03:32 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 3093ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240729/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-don't-overlap.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*; 
+
+class Line implements Comparable<Line>{
+    long start, end;
+
+    public Line(long start, long end){
+        this.start = start;
+        this.end = end; 
+    }
+
+    @Override
+    public int compareTo(Line o){
+        if(this.start == o.start){
+            return this.end>o.end ? 1: -1; 
+        }else{
+            return this.start>o.end? 1:-1; 
+        }
+    }
+}
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+
+
+    public static void main(String[] args) throws IOException{
+        tokens = new StringTokenizer(buffer.readLine()); 
+
+        int n = Integer.parseInt(tokens.nextToken());
+        int m = Integer.parseInt(tokens.nextToken()); 
+
+        Line[] lines = new Line[m];
+
+        for(int i=0; i<m; i++){
+            tokens = new StringTokenizer(buffer.readLine()); 
+            lines[i] = new Line(Integer.parseInt(tokens.nextToken()), Integer.parseInt(tokens.nextToken()));
+        }
+
+        Arrays.sort(lines);
+
+        long l = 0;
+        long h = (long) 1e18;
+
+        while(h>l){
+            long mid = (h+l+1)/2; 
+
+            if(isValid(mid, lines, n)){
+                l = mid; 
+            }else{
+                h = mid-1; 
+            }
+        }
+
+        System.out.println(l);
+    }
+    private static boolean isValid(long mid, Line[] lines, int n){
+        long lastPosition = lines[0].start; 
+        int cnt = 1; 
+        for(int i=0; i<lines.length; i++){
+            Line line = lines[i];
+            for(long test=line.start; test<=line.end; test++){
+                if(test-lastPosition>=mid){
+                    cnt++; 
+                    lastPosition = test; 
+                }
+            }
+        }
+
+        return cnt>=n; 
+    }
+}
