02c111ba339dbf06bb7db95be25d2b19b5867e77
HanJu <119592507+dev1week@users.noreply.github.com>
2024-08-06 14:53:45 +0900

    Title: 동전 거슬러주기, Time: 263ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240806/동전 거슬러주기/coin-change.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+
+    public static void main(String[] args) throws IOException{
+        tokens = new StringTokenizer(buffer.readLine()); 
+        int n = Integer.parseInt(tokens.nextToken());
+        int m = Integer.parseInt(tokens.nextToken()); 
+
+        int[] coins = Arrays.stream(buffer.readLine().split("\\s++")).mapToInt(Integer::parseInt).toArray(); 
+        
+        int[] dp = new int[m+1];
+
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int sum=1; sum<=m; sum++){
+            for(int coin :coins){
+                if(sum-coin<0) continue; 
+                dp[sum] = Math.min(dp[sum], dp[sum-coin]+1);
+            }
+
+        }
+
+        System.out.println(dp[m]); 
+
+
+    }
+}
