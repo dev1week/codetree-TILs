34c7d5f8853cae9348c0ead15ecae5b1409429d3
HanJu <119592507+dev1week@users.noreply.github.com>
2024-08-06 09:51:14 +0900

    Title: 최대 점프 횟수, Time: 260ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240806/최대 점프 횟수/maximum-number-of-jumps.java
+++ 240806/최대 점프 횟수/maximum-number-of-jumps.java
@@ -11,15 +11,20 @@ public class Main {
         int[]  numbers = Arrays.stream(buffer.readLine().split("\\s++")).mapToInt(Integer::parseInt).toArray();
         int[] d = new int[n]; 
 
+        Arrays.fill(d, -1);
+        d[0] = 0; 
+
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+            for(int j=0; j<i; j++){
                 if(numbers[j]+j>=i){
+                    if(d[j]==-1) continue;   
                     d[i] = d[j]+1; 
+                    
                 }
 
             }
         }
-
+        System.out.println(Arrays.toString(d)); 
         System.out.println(Arrays.stream(d).max().getAsInt()); 
         
     }
