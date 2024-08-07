6b0caeda2638a6998b1d844fd08ba657a813332b
HanJu <119592507+dev1week@users.noreply.github.com>
2024-08-07 14:57:31 +0900

    Title: 부분 수열의 합이 m, Time: 241ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240807/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens; 
+
+    public static void main(String[] args) throws IOException{
+        tokens = new StringTokenizer(buffer.readLine());
+         
+        int n = Integer.parseInt(tokens.nextToken());
+        int m = Integer.parseInt(tokens.nextToken());
+
+        int[] numbers = Arrays.stream(buffer.readLine().split("\\s++")).mapToInt(Integer::parseInt).toArray(); 
+        int[] d = new int[m+1]; 
+        //d[sum] = 부분 수열의 총합이 sum일 때 가능한 최소 수열의 길이 
+
+        Arrays.fill(d, 101);
+
+        d[0] = 0;
+
+        for(int number: numbers){
+            for(int sum=m; sum>=0; sum--){
+                if(sum-number<0) continue; 
+                if(d[sum-number]==101) continue;
+                d[sum] = Math.min(d[sum], d[sum-number]+1); 
+            }
+        }
+
+        System.out.println(d[m]);
+        
+    }
+}
