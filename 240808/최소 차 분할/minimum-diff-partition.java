6ec6a936da380d189503724836990e7989a8b648
HanJu <119592507+dev1week@users.noreply.github.com>
2024-08-08 10:40:35 +0900

    Title: 최소 차 분할, Time: 119ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240808/최소 차 분할/minimum-diff-partition.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+
+    public static void main(String[] args) throws IOException{
+
+        int n = Integer.parseInt(buffer.readLine());
+        int[] numbers = new int[n];
+
+        tokens = new StringTokenizer(buffer.readLine());
+        int tot = 0; 
+        for(int i=0; i<n; i++){
+            numbers[i] = Integer.parseInt(tokens.nextToken()); 
+            tot+=numbers[i]; 
+        }
+
+        int[] d = new int[tot+1]; 
+        Arrays.fill(d,-1); 
+        d[0] = 0; 
+
+        for(int number: numbers){
+            for(int sum=tot; sum>=0; sum--){
+                if(sum-number<0)continue; 
+                if(d[sum-number]==-1)continue; 
+                d[sum] = Math.max(d[sum], d[sum-number]+1);
+            }
+        }
+
+        System.out.println(Arrays.toString(d)); 
+
+        int result = Integer.MAX_VALUE;
+        for(int sum=1; sum<=tot; sum++){
+            if(d[sum]!=-1){
+                result = Math.min(result, tot-sum);
+            }
+        }
+        System.out.println(result); 
+        
+        
+    }
+}
