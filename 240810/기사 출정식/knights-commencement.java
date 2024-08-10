638ede31d22cc561dcdab021800f7da377ab11eb
HanJu <119592507+dev1week@users.noreply.github.com>
2024-08-10 23:50:44 +0900

    Title: 기사 출정식, Time: 3041ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

---
+++ 240810/기사 출정식/knights-commencement.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*; 
+
+class Knight{
+    int id;
+
+    Knight prev, next; 
+
+    public Knight(int id){
+        this.id = id; 
+    }
+
+    @Override
+    public String toString(){
+        if(this.prev==null||this.next==null) return "test"; 
+        return this.next.id+" "+this.prev.id; 
+    }
+
+}
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens; 
+
+    public static void main(String[] args) throws IOException{
+
+        tokens = new StringTokenizer(buffer.readLine());
+
+        int n = Integer.parseInt(tokens.nextToken());
+        int m = Integer.parseInt(tokens.nextToken());
+
+        Map<Integer, Knight> knights = new HashMap<>(); 
+
+        
+
+        int[] ids = Arrays.stream(buffer.readLine().split("\\s++")).mapToInt(Integer::parseInt).toArray(); 
+        for(int i=1; i<=Arrays.stream(ids).max().getAsInt(); i++){
+            knights.put(i, new Knight(i));
+        }
+
+        for(int i=0; i<n-1; i++){
+            connect(knights.get(ids[i]), knights.get(ids[i+1])); 
+        }
+        connect(knights.get(ids[n-1]), knights.get(ids[0])); 
+
+        // System.out.println(current.id);
+
+        // System.out.println(Arrays.toString(knights));
+
+
+        StringBuilder result = new StringBuilder(); 
+        for(int query=0; query<m; query++){
+
+            int call = Integer.parseInt(buffer.readLine());
+            result.append(knights.get(call)).append("\n"); 
+            delete(knights.get(call));
+        }
+
+        System.out.println(result); 
+        
+    }
+
+    private static void connect(Knight s, Knight e){
+        if(s!=null)s.next = e;
+        if(e!=null)e.prev = s; 
+    }
+
+    private static void delete(Knight n){
+        connect(n.prev, n.next); 
+        n.next=n.prev=null;
+    }
+}
