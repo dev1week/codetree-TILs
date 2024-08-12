0ed2ae0835ad6f96231933479202d966f7a84f7b
HanJu <119592507+dev1week@users.noreply.github.com>
2024-08-12 11:29:15 +0900

    Title: 정수 분배하기, Time: 79ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240812/정수 분배하기/distributing-integers.java
@@ -0,0 +1,42 @@
+import java.io.*; 
+import java.util.*; 
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens; 
+    public static void main(String[] args) throws IOException{
+        tokens = new StringTokenizer(buffer.readLine());
+
+        int n = Integer.parseInt(tokens.nextToken());
+        int m = Integer.parseInt(tokens.nextToken()); 
+
+        int[] numbers = new int[n]; 
+        int maxNum = 0; 
+        for(int i=0; i<n; i++){
+            numbers[i] = Integer.parseInt(buffer.readLine()); 
+            maxNum = Math.max(maxNum, numbers[i]);
+        }
+
+        int h = maxNum; 
+        int l = 1;
+
+        while(h>l){
+            int mid = (h+l+1)/2;
+            if(isValid(mid, numbers, m )){
+                l = mid;
+            }else{
+                h = mid-1; 
+            }
+        }
+        System.out.println(h); 
+    }
+
+    private static boolean isValid(int mid, int[] numbers, int limit){
+        int count = 0; 
+        for(int number: numbers){
+            count += number/mid;
+        }
+
+        return count>=limit;
+
+    }
+}
