58685b1cc6d118dc87b8a85aa51ed849d72a41c7
HanJu <119592507+dev1week@users.noreply.github.com>
2024-09-20 14:10:21 +0900

    Title: 자연수 n개의 합, Time: 3010ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

---
+++ 240920/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*; 
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens;
+
+    public static void main(String[] args) throws IOException{
+        long limit = Long.parseLong(buffer.readLine()); 
+
+
+        long h = limit/2+1;
+        long l = 1; 
+
+        //가능한 n을 이진탐색으로 뽑아본다. 
+            //뽑은 n을 가지고 총합을 계산하고 limit 이하인지 확인한다. 
+
+        //최댓값 구하기 
+
+        while(h>l){
+            long mid = (l+h+1)/2; 
+
+            if(isValid(mid, limit)){
+                l = mid;
+            }else{
+                h = mid-1; 
+            }
+        }
+
+        System.out.println(l);
+    }
+
+    private static boolean isValid(long mid, long limit){
+        int sum = 0;
+
+        for(int i=1; i<=mid; i++){
+            sum +=i;
+
+            if(sum>limit){
+                return false;
+            }
+        }
+
+        return sum<=limit; 
+
+
+    }
+}
