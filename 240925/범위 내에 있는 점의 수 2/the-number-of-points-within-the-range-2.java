da8e2c4322a946f1e8e88ecdeaef27cbb531332f
HanJu <119592507+dev1week@users.noreply.github.com>
2024-09-25 11:53:28 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240925/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*; 
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens; 
+
+    private static final int MAX_SIZE = (int) 1e6; 
+
+    public static void main(String[] args) throws IOException{
+           
+
+        tokens = new StringTokenizer(buffer.readLine());
+        int n = Integer.parseInt(tokens.nextToken());
+        int q = Integer.parseInt(tokens.nextToken()); 
+
+        int[] points = Arrays.stream(buffer.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        int maxPoint = Arrays.stream(points).max().getAsInt();
+        int[] pointCnts = new int[MAX_SIZE+1];
+        int[] pointPrefixSum = new int[MAX_SIZE+1];
+        for(int point : points){
+            pointCnts[point]++; 
+        }
+        //System.out.println(Arrays.toString(pointCnts)); 
+        //n개 점의 위치를 받아 카운팅함.  - o(n) 
+
+        //카운팅 배열을 토대로 누적합을 구한다. - o(n)
+
+        for(int i=1; i<=maxPoint; i++){
+            pointPrefixSum[i] = pointPrefixSum[i-1] + pointCnts[i];
+        }
+
+        
+
+        //쿼리를 받으며 바로바로 누적합으로 계산해낸다. o(1 * q) 
+        StringBuilder result = new StringBuilder(); 
+        for(int query=0; query<q; query++){
+            tokens = new StringTokenizer(buffer.readLine());
+
+            int start = Integer.parseInt(tokens.nextToken());
+            int end = Integer.parseInt(tokens.nextToken()); 
+
+            result.append(getPartSum(start, Math.min(end, maxPoint), pointPrefixSum)).append("\n");
+
+        }
+
+        System.out.println(result);
+    }
+
+    private static int getPartSum(int start, int end, int[] prefixSum){
+        return prefixSum[end] - prefixSum[start-1]; 
+    }
+}
