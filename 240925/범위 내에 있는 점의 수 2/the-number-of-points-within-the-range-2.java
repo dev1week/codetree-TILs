8f3d13f53c1241c971e5a35f5d804d3d4bcd5e39
HanJu <119592507+dev1week@users.noreply.github.com>
2024-09-25 11:56:59 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 963ms, Memory: 48MB, Status: Passed - CodeTree

--- 240925/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240925/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -30,6 +30,10 @@ public class Main {
             pointPrefixSum[i] = pointPrefixSum[i-1] + pointCnts[i];
         }
 
+        for(int i=maxPoint+1; i<=MAX_SIZE; i++){
+            pointPrefixSum[i] = pointPrefixSum[i-1];
+        }
+
         
 
         //쿼리를 받으며 바로바로 누적합으로 계산해낸다. o(1 * q) 
@@ -40,14 +44,14 @@ public class Main {
             int start = Integer.parseInt(tokens.nextToken());
             int end = Integer.parseInt(tokens.nextToken()); 
 
-            result.append(getPartSum(start, Math.min(end, maxPoint), pointPrefixSum)).append("\n");
+            result.append(getPartSum(start, end, pointPrefixSum, pointCnts)).append("\n");
 
         }
 
         System.out.println(result);
     }
 
-    private static int getPartSum(int start, int end, int[] prefixSum){
-        return prefixSum[end] - prefixSum[start-1]; 
+    private static int getPartSum(int start, int end, int[] prefixSum, int[] pointCnts){
+        return prefixSum[end] - prefixSum[start]+pointCnts[start]; 
     }
 }
