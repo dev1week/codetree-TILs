7b84ad61b5b0dcb648aa8aba015547ce200fccb5
HanJu <119592507+dev1week@users.noreply.github.com>
2024-09-30 11:38:57 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 538ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240930/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens; 
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        int n = Integer.parseInt(buffer.readLine()); 
+        
+        
+        int[] numbers = Arrays.stream(buffer.readLine().split("\\s++")).mapToInt(Integer::parseInt).toArray(); 
+
+
+        
+        //현재 최대합
+        int result = Integer.MIN_VALUE;
+        //현재 구간합 
+        int partSum = 0; 
+        for(int i=0; i<n; i++){
+            
+            //현재 i로 구간합을 구해본다. 
+            int tmp = partSum + numbers[i];
+            //구간합이 음수가 되었다. 
+            if(tmp<0){
+                partSum = numbers[i]; 
+            }else{
+                partSum += numbers[i];
+                
+            }
+            result = Math.max(partSum, result); 
+            
+            // 구간합이 
+
+        }
+        System.out.println(result);
+
+    }
+}
