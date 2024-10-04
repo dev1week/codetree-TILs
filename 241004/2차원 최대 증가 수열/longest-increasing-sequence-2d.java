e3b6df6e0e1a264f8dd3b0ed1644a427edb7ca81
HanJu <119592507+dev1week@users.noreply.github.com>
2024-10-04 22:51:11 +0900

    Title: 2차원 최대 증가 수열, Time: 138ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241004/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*; 
+public class Main {
+    static BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer tokens; 
+
+    public static void main(String[] args) throws IOException{
+        tokens = new StringTokenizer(buffer.readLine());
+        int n = Integer.parseInt(tokens.nextToken());
+        int m = Integer.parseInt(tokens.nextToken()); 
+
+        int[][] map = new int[n][m];
+        int[][] dp = new int[n][m]; 
+
+        for(int x=0;x<n; x++){
+            tokens = new StringTokenizer(buffer.readLine()); 
+            for(int y=0; y<m; y++){
+                map[x][y] = Integer.parseInt(tokens.nextToken()); 
+            }
+        }
+
+        dp[0][0] = 1; 
+
+        for(int currentX=1; currentX<n; currentX++){
+            for(int currentY=1; currentY<m; currentY++){
+                for(int prevX=0; prevX<currentX; prevX++){
+                    for(int prevY=0; prevY<currentY; prevY++){
+                        if(map[prevX][prevY]==0)continue; 
+                        if(map[prevX][prevY]<map[currentX][currentY]){
+                            dp[currentX][currentY] = Math.max(dp[currentX][currentY], dp[prevX][prevY]+1);
+                        }
+                    }
+                }
+            }
+        }
+        //print(dp); 
+
+        int result = 0 ; 
+        for(int x=0; x<n; x++){
+            for(int y=0; y<m; y++){
+                result = Math.max(result, dp[x][y]);
+            }
+        }
+
+        System.out.println(result); 
+
+
+    }
+
+    private static void print(int[][] map){
+        for(int[] ma: map){
+            for(int m : ma){
+                System.out.print(m);
+            }System.out.println(); 
+        }
+    }
+}
