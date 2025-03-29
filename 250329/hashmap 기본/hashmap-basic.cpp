00fd4542a9dc0410d2ff88d1a52fe667d6751a14
HanJu <119592507+dev1week@users.noreply.github.com>
2025-03-29 14:29:44 +0900

    Title: hashmap 기본, Time: 57ms, Memory: 4MB, Status: Passed - Codetree

---
+++ 250329/hashmap 기본/hashmap-basic.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include<unordered_map>
+
+using namespace std;
+
+int n;
+string cmd[100000];
+int k[100000];
+int v[100000];
+
+int main() {
+    cin >> n;
+    unordered_map<int, int> m; 
+
+    for (int i = 0; i < n; i++) {
+        cin >> cmd[i];
+        cin >> k[i];
+        if (cmd[i] == "add") {
+            cin >> v[i];
+            m[k[i]] = v[i]; 
+        }else if(cmd[i]=="remove"){
+            m.erase(k[i]);
+        }else if(cmd[i]=="find"){
+            if(m.find(k[i])==m.end()){
+                cout << "None" << endl;
+            }else{
+                cout << (m.find(k[i]))->second << endl; 
+            }
+        }
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
