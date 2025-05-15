6094b3c4b910747c07aa4cb534fdbc1f4bbdd5c0
HanJu <119592507+dev1week@users.noreply.github.com>
2025-05-15 10:08:22 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 186ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250515/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.js
@@ -0,0 +1,10 @@
+let fs = require("fs")
+
+let arr = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+for(let i=0; i<arr.length; i++){
+    if(arr[i]==0){
+        console.log(arr[i-1]+arr[i-2]+arr[i-3])
+        break;
+    }
+}
