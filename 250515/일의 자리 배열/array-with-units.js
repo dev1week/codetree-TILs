dd4963027ff6a1cce75705072e2a5a86cef8511f
HanJu <119592507+dev1week@users.noreply.github.com>
2025-05-15 11:09:46 +0900

    Title: 일의 자리 배열, Time: 185ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250515/일의 자리 배열/array-with-units.js
@@ -0,0 +1,15 @@
+let fs = require("fs")
+
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+let arr = Array(10)
+
+arr[0] = input[0]
+arr[1] = input[1]
+
+for(let i=2; i<10; i++){
+    arr[i] = (arr[i-1]+arr[i-2])%10;
+}
+
+console.log(arr.join(" "))
+
