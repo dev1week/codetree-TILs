e795b468f485b1785cd4ec39a95262dee4661f01
HanJu <119592507+dev1week@users.noreply.github.com>
2025-05-16 19:36:27 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 214ms, Memory: 47MB, Status: Passed - Codetree

--- 250516/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
+++ 250516/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -5,8 +5,9 @@ let [n, m] = input[0].split(" ").map(Number);
 function square(n,m){
     for(let i=0; i<n; i++){
         for(let j=0; j<m; j++){
-            process.stdout.write(d+'\n');
+            process.stdout.write('1');
         }
+        process.stdout.write('\n');
     }
 }
 
