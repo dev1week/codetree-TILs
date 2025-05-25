c5e0217ef28149d0eac9592fdb688fd8c11399ef
HanJu <119592507+dev1week@users.noreply.github.com>
2025-05-25 09:33:42 +0900

    Title: 더 긴 문자열, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250525/더 긴 문자열/longer-string.js
+++ 250525/더 긴 문자열/longer-string.js
@@ -5,6 +5,6 @@ let string = fs.readFileSync(0).toString().trim().split(" ")
 if(string[0].length===string[1].length){
     console.log("same")
 }else{
-    console.log(string[0].length>string[1].length?string[0]+" "+(string[0].length+1):string[1]+" "+(string[0].length+1))
+    console.log(string[0].length>string[1].length?string[0]+" "+string[0].length:string[1]+" "+(string[1].length))
 }
 
