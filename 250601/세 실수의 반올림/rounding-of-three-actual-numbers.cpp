9b3e4294e586f4bc76d88bec3157542101c2d69a
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-01 15:11:48 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250601/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a,b,c;
+
+    ci>>a>>b>>c; 
+
+    cout.precision(3);
+    cout<<fixed; 
+    cout<<a<<"\n"<<b<<"\n"<<c;
+    return 0;
+}
