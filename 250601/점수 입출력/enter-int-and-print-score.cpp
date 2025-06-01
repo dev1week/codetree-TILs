de328cf4f0d490e923ecfb51911b0fb178c0cb67
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-01 14:54:14 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250601/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n = 0;
+    cin >> n;
+    cout << "Your score is ";
+    cout << n;
+    cout << " point.";
+    return 0;
+}
