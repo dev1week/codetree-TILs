b8d3a946be0aaf085816808ecc70b3f59abdea81
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-07 22:22:11 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int n){
+    for(int i=0; i<n; i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+}
+
+int N;
+
+int main() {
+    cin >> N;
+
+    print(N);
+
+    // Please write your code here.
+
+    return 0;
+}
+
