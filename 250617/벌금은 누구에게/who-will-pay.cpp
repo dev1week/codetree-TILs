b44bc961d0d1ef5dcf4ae1b7fdfbe19f5ff56807
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-17 09:50:45 +0900

    Title: 벌금은 누구에게, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250617/벌금은 누구에게/who-will-pay.cpp
+++ 250617/벌금은 누구에게/who-will-pay.cpp
@@ -5,15 +5,21 @@ int main() {
     int n,m,k; 
     cin>>n>>m>>k; 
     int cnt[n+1] ={}; 
+    bool flag = true; 
 
     for(int i=0; i<m; i++){
         int student = 0;
         cin>>student; 
         cnt[student]++; 
         if(cnt[student]>=k){
+            flag = false; 
             cout<<student; 
             break;
         }
+    }
+
+    if(flag){
+        cout<<-1; 
     }
 
     return 0;
