b6cc51c4505c57559623b535067d4a6148c4099f
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-21 16:28:51 +0900

    Title: 만나는 그 순간, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250621/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector> 
+using namespace std;
+int not_visited = 10000; 
+
+
+void input(int time[], char dir[], int n){
+    for(int i=0; i<n; i++){
+        cin>>dir[i]>>time[i];
+    }
+}
+
+vector<int> get_position(int time[], char dir[], int n){
+    //시간별 a의 위치 저장하기 
+    vector<int> position(1001, not_visited);
+    int total_time = 0; 
+    int current_position = 0; 
+    for(int i=0; i<n; i++){
+        int end = time[i]; 
+        int d = dir[i]=='L'?-1:1; 
+        for(int t=1; t<=end; t++){
+            total_time++; 
+            current_position += (d); 
+            position[total_time] = current_position; 
+        }
+    }
+
+
+    return position; 
+    
+}
+
+int get_meeting_time(vector<int> position_a, vector<int> position_b){
+
+    for(int time=1; time<=1000; time++){
+        if(position_a[time]==position_b[time]){
+            return time; 
+        }
+
+        if(position_a[time]==not_visited||position_b[time]==not_visited){
+            return -1; 
+        }
+    }
+
+    return -1; 
+}
+
+int main() {
+    int n, m; 
+
+    cin>>n>>m; 
+
+
+    int time_a[n] = {};
+    char dir_a[n] = {}; 
+
+    int time_b[m] = {};
+    char dir_b[m] = {}; 
+
+    input(time_a, dir_a, n); 
+    input(time_b, dir_b, m); 
+
+
+    vector<int> position_a = get_position(time_a, dir_a, n); 
+    vector<int> position_b = get_position(time_b, dir_b, m);
+
+    cout<<get_meeting_time(position_a, position_b);
+
+}
