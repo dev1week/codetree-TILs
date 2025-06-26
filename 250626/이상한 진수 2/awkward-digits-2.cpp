7924d84842f35827a562d277e33f6b6bd9076039
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-26 16:32:05 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250626/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int convert_digit(string &data){
+    int part = 1; 
+    int result = 0; 
+
+    for(int i=data.length()-1; i>=0; i--){
+        result += (data[i]-'0')*part; 
+        part*=2; 
+    }
+
+    return result; 
+
+}
+
+char convert(int n){
+    return n=='0'?'1':'0'; 
+}
+int main() {
+    string a = "";
+    cin >> a;
+
+    int result = 0; 
+
+    for(int i=0;i<a.length(); i++){
+        a[i] = convert(a[i]);
+        int number = convert_digit(a);
+        a[i] = convert(a[i]);
+
+        if(result<number){
+            result = number; 
+        }
+    }
+
+    cout<<result; 
+    // Please write your code here.
+
+    return 0;
+}
