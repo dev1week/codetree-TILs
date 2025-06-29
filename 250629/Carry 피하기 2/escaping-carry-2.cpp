4868b4cde2453802b73a0e40615c5dc389fdaee9
HanJu <119592507+dev1week@users.noreply.github.com>
2025-06-29 22:36:08 +0900

    Title: Carry 피하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/Carry 피하기 2/escaping-carry-2.cpp
+++ 250629/Carry 피하기 2/escaping-carry-2.cpp
@@ -10,10 +10,11 @@ bool is_valid(int n1, int n2, int n3){
     
     int offset = 10;
 
-    while(offset<=10000){
+    while(n1!=0||n2!=0||n3!=0){
         int tmp = n1%offset+n2%offset+n3%offset; 
-        offset *= 10; 
-        //cout<<tmp<<endl;
+        n1 /=10;
+        n2 /=10;
+        n3 /=10;
         if(tmp>9) return false; 
     }
 
