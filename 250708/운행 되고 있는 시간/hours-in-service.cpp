8c887fd280f5526f69eda0c53ec61f19b0c8454f
HanJu <119592507+dev1week@users.noreply.github.com>
2025-07-08 15:55:36 +0900

    Title: 운행 되고 있는 시간, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250708/운행 되고 있는 시간/hours-in-service.cpp
+++ 250708/운행 되고 있는 시간/hours-in-service.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int N;
 int A[101], B[101];
 
+void change_log(int start, int end, int log[], int d){
+    for(int time=start; time<end; time++){
+        log[time]+=d; 
+    }
+}
+
 int main() {
     cin >> N;
 
@@ -12,18 +18,19 @@ int main() {
     int log[1001] = {};
     for (int i = 1; i <= N; i++) {
         cin >> A[i] >> B[i];
-        for(int time=A[i]; time<=B[i]; time++){
-            log[time] = i; 
-        }
+        change_log(A[i], B[i], log, 1); 
     }
+
+
+
     int result = 0; 
     for(int skip=1; skip<=N; skip++){
         int cnt = 0; 
+        change_log(A[skip], B[skip],log, -1); 
         for(int time=1; time<=1000; time++){
-            if(log[time]==skip)continue;
             if(log[time]>0)cnt++; 
         }
-
+        change_log(A[skip], B[skip],log, 1); 
         result = max(cnt, result); 
     }
 
