1d1189630c66bb476ca85bbca5ada841804b744c
HanJu <119592507+dev1week@users.noreply.github.com>
2025-07-08 15:27:31 +0900

    Title: 운행 되고 있는 시간, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250708/운행 되고 있는 시간/hours-in-service.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+int N;
+int A[101], B[101];
+
+int main() {
+    cin >> N;
+
+
+    int log[1001] = {};
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i] >> B[i];
+        for(int time=A[i]; time<=B[i]; time++){
+            log[time] = i; 
+        }
+    }
+    int result = 0; 
+    for(int skip=1; skip<=N; skip++){
+        int cnt = 0; 
+        for(int time=0; time<=1000; time++){
+            if(log[time]==skip)continue;
+            if(log[time]>0)cnt++; 
+        }
+
+        result = max(cnt, result); 
+    }
+
+    cout<<result; 
+
+    // Please write your code here.
+
+    return 0;
+}
