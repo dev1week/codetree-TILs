02a56359aefac0283c407bfdd4cfbb98e921b372
HanJu <119592507+dev1week@users.noreply.github.com>
2025-08-11 23:10:54 +0900

    Title: 아름다운 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250811/아름다운 수/beautiful-number.cpp
+++ 250811/아름다운 수/beautiful-number.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector> 
+#include <functional> 
 using namespace std;
 
 
@@ -10,27 +11,16 @@ void print(const vector<int>&seq){
     cout<<endl; 
 }
 
-bool is_beautiful(vector<int>&seq){
-    for(int i=0; i<seq.size(); i++){
-        int current = seq[i]; 
-        int cnt = 0; 
-        for(int offset=0; offset<current; offset++){
-            if(current==seq[i+offset])cnt++; 
-        }
-        if(cnt!=current) return false; 
-        i += (current-1); 
-    }
-    return true; 
-}
 
-int count_beautiful(vector<int>&seq, int current, int n){
+int count_seq_matching_condition(vector<int>&seq, int current, int n,
+    const function<bool(vector<int>&)>& is_count){
     if(current==n){
-        return is_beautiful(seq)?1:0; 
+        return is_count(seq)?1:0; 
     }
     int result = 0; 
     for(int next=1; next<=4; next++){
         seq[current] = next; 
-        result += count_beautiful(seq, current+1, n); 
+        result += count_seq_matching_condition(seq, current+1, n, is_count); 
     }
 
     return result; 
@@ -41,7 +31,22 @@ int main() {
     cin >> n;
     vector<int> sequence(n); 
     //1~4 중 n자리 순열을 구한다. 
-    int result = count_beautiful(sequence, 0, n); 
+
+    auto is_beaultiful = [&](vector<int>&seq)->bool{
+        for(int i=0; i<seq.size(); i++){
+            int current = seq[i]; 
+            int cnt = 0; 
+            for(int offset=0; offset<current; offset++){
+                if(current==seq[i+offset])cnt++; 
+            }
+            if(cnt!=current) return false; 
+            i += (current-1); 
+        }
+        return true; 
+    };
+
+
+    int result = count_seq_matching_condition(sequence, 0, n, is_beaultiful); 
     cout<<result;
     return 0;
 }
