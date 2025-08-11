5cab4e9b4dfdd4f8090fbf3c5d22fad4897408eb
HanJu <119592507+dev1week@users.noreply.github.com>
2025-08-11 22:25:36 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 133ms, Memory: 1MB, Status: Passed - Codetree

--- 250811/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.cpp
+++ 250811/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -9,30 +9,22 @@ void print(vector<int>&result){
     cout<<endl; 
 }
 
-void choose(vector<int>&result, int k, int n){
-    if(result.size()==n){
+void choose(vector<int>&result, int k, int n, int current){
+    if(current==n){
         print(result); 
         return; 
     }
-
     for(int i=1; i<=k; i++){
-        result.push_back(i);
-        choose(result, k,n); 
-        result.pop_back(); 
+        result[current] = i; 
+        choose(result, k,n, current+1); 
     }
-
-
 }
 
 
 int main() {
     int K, N;
-
     cin >> K >> N;
-
-    vector<int> result; 
+    vector<int> result(N,0); 
     //k까지의 수 중 n번 뽑는다. 
-    choose(result, K,N); 
-
-
+    choose(result, K,N, 0); 
 }
